c6d4d3becef3f8b1cee3fe5b68805a095738fd6f
Nathan Pabst <nathan.pabst@gmail.com>
2018-05-03 21:31:18 -0500

    wip

--- javascripts/dataGatekeeper.js
+++ javascripts/dataGatekeeper.js
@@ -1,7 +1,7 @@
 const xhr = require('./xhr');
 const data = require('./data');
 const categoryDom = require('./categoryDom');
-const elemDom = require('./elementsDom');
+// const elemDom = require('./elementsDom');
 
 const onCategoryLoad = function () {
   const categoryData = JSON.parse(this.responseText).categories;
@@ -9,18 +9,18 @@ const onCategoryLoad = function () {
   categoryDom(categoryData);
 };
 
-const onMovieElementLoad = function () {
-  const movieData = JSON.parse(this.responseText).movies;
-  data.setElements(movieData);
-  elemDom(movieData);
-};
+// const onMovieElementLoad = function () {
+//   const movieData = JSON.parse(this.responseText).movies;
+//   data.setElements(movieData);
+//   elemDom(movieData);
+// };
 
 const badThings = function () {
   console.error('dang friggin crap!');
 };
 
 const initializer = () => {
-  xhr.loadMovieElements(onMovieElementLoad, badThings);
+  // xhr.loadMovieElements(onMovieElementLoad, badThings);
   xhr.loadCategories(onCategoryLoad, badThings);
 };
 
